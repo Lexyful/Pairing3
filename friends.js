244fd94464ec6d0d9d1b3e2b2e208aeb80447557
Alexys Jordan <alexyskjordan@gmail.com>
2022-11-10 16:02:50 -0700

    Add another array for statement

--- friends.js
+++ friends.js
@@ -1,6 +1,11 @@
 var number = [13, 7, 15, 30, 45, 5, 34, 89]
-for(i=0; i<8;i++) {
+for (i=0; i<8;i++) {
     if (number[i]>30) {
         console.log(number[i]);
+    }
+}
+for (i=0; i<8;i++) {
+    if (number[i]<60) {
+    console.log(number[i]);
     }
 }
